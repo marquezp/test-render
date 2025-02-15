05fe1054c4175f53765815e1b181a664ee392bdf
Jorge Marquez Peralta <marquezp@ualberta.ca>
2025-02-15 15:20:52 -0700

    set up static file serving

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ let persons = [
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("dist"));
 app.use(requestLogger);
 app.use(morgan(":method :url :status - :response-time ms :body"));
 
